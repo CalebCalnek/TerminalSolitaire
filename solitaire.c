923f12e90aa9a1b84edeb1665f5451ee74a7533e
CalebCalnek <calebgcc156@gmail.com>
2024-10-20 16:12:28 -0600

    add exit on mouse click

--- solitaire.c
+++ solitaire.c
@@ -2,16 +2,30 @@
 
 int main(void) {
 	initscr();
+	keypad(stdscr, TRUE);
+	noecho();
+
+	/* mouse settup */
+	mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
+	mouseinterval(0);
 
 	/* color */
 	start_color();
-	init_pair(1, COLOR_BLACK, COLOR_BLUE);
-
-	clear();
+	init_pair(1, COLOR_WHITE, COLOR_BLUE);
 	bkgd(COLOR_PAIR(1));
 
 	refresh();
-	getch();
+
+	MEVENT event;
+	while (1) {
+		if (getch() == KEY_MOUSE) {
+			if (getmouse(&event) == OK) {
+				if (event.bstate & BUTTON1_PRESSED) {
+					break;
+				}
+			}
+		}
+	}
 
 	endwin();
 	return 0;
