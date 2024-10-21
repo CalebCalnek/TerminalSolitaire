e414985ea98823d93c8236388432788c19652c76
CalebCalnek <calebgcc156@gmail.com>
2024-10-20 18:16:38 -0600

    add mouse release detection

--- solitaire.c
+++ solitaire.c
@@ -1,9 +1,20 @@
 #include <curses.h>
 
+int card_x, card_y;
+
+void draw(void) {
+	clear();
+	attron(COLOR_PAIR(2));
+	mvprintw(card_y, card_x, " ");
+	attroff(COLOR_PAIR(2));
+}
+
 int main(void) {
 	initscr();
 	keypad(stdscr, TRUE);
 	noecho();
+
+	curs_set(0);
 
 	/* mouse settup */
 	mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
@@ -12,9 +23,15 @@ int main(void) {
 	/* color */
 	start_color();
 	init_pair(1, COLOR_WHITE, COLOR_BLUE);
+	init_pair(2, COLOR_BLACK, COLOR_RED);
 	bkgd(COLOR_PAIR(1));
 
+	card_x = 0;
+	card_y = 1;
+
 	refresh();
+
+	draw();
 
 	int ch;
 	MEVENT event;
@@ -23,8 +40,12 @@ int main(void) {
 		if (ch == KEY_MOUSE) {
 			if (getmouse(&event) == OK) {
 				if (event.bstate & BUTTON1_PRESSED) {
-					clear();
+					draw();
 					mvprintw(0, 0, "mouseX: %d; mouseY: %d", event.x, event.y);
+				} else if (event.bstate & BUTTON1_RELEASED) {
+					if (event.x == card_x && event.y == card_y) {
+						mvprintw(2, 0, "clicked card");
+					}
 				}
 			}
 		}
