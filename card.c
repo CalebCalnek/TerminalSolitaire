7cfd23586f35c5cd768d85b0e07fdb4f9bf37c94
CalebCalnek <calebgcc156@gmail.com>
2024-10-21 20:10:18 -0600

    set card data mappings

--- card.c
+++ card.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
 enum suit {
 	CLUBS,
 	SPADES,
@@ -6,23 +10,46 @@ enum suit {
 };
 
 enum rank {
-	ACE = 1,
-	TWO = 2,
-	THREE = 3,
-	FOUR = 4,
-	FIVE = 5,
-	SIX = 6,
-	SEVEN = 7,
-	EIGHT = 8,
-	NINE = 9,
-	TEN = 10,
-	JACK = 11,
-	QUEEN = 12,
-	KING = 13,
+	ACE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE,
+	TEN,
+	JACK,
+	QUEEN,
+	KING,
 };
+
+// { ♣, ♠, ♥, ♦ };
+wchar_t suit_chars[4] = { 0x2663, 0x2660, 0x2665, 0x2666 };
+char *rank_chars[13] = { "A", "2", "3", "4", "5", "6", "7",
+	"8", "9", "10", "J", "Q", "K" };
 
 struct card {
 	enum suit suit;
-	enum rank value;
+	enum rank rank;
 	int x, y;
 };
+
+void print_card(struct card c) {
+	wchar_t suit;
+	char *rank;
+	suit = suit_chars[c.suit];
+	rank = rank_chars[c.rank];
+	wprintf(L"%s %lc\n", rank, suit);
+}
+
+int main() {
+	struct card mycard;
+	setlocale(LC_ALL, "");
+
+	mycard.suit = DIAMONDS;
+	mycard.rank = KING;
+
+	print_card(mycard);
+}
