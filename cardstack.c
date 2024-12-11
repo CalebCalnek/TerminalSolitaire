a80c608e08682d28001a1660fff8a4b8b36aaf3c
CalebCalnek <calebgcc156@gmail.com>
2024-12-11 15:19:28 -0600

    start function for drawing empty stack

--- cardstack.c
+++ cardstack.c
@@ -38,12 +38,28 @@ void push(struct cardstack basestack, struct cardstack newstack) {
 	basestack.top = newstack.top;
 }
 
+void draw_empty_stack(int stack_i, int card_count) {
+	int x = STACK_SPACING * (stack_i + 1) + CARD_WIDTH * stack_i;
+	int y = 1 + card_count;
+	for (int i = 0; i < CARD_HEIGHT; i++) {
+		for (int j = 0; j < CARD_WIDTH; j++) {
+			mvaddstr(y + i, x + j, "a");
+		}
+	}
+}
+
 void draw_stack(struct cardstack stack) {
 	struct card *card_i;
 	int card_count;
 
 	card_i = stack.bottom;
 	card_count = 0;
+
+	if (card_i == NULL) {
+		draw_empty_stack(stack.index, card_count);
+		return;
+	}
+
 	while (card_i != NULL) {
 		draw_card(*card_i, stack.index, card_count);
 		card_i = card_i->next;
